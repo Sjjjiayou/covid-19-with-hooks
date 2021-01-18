63c01ddd98ab56868f9ba85c159c41a00c20f48f
songjj7253 <sjj10200104@dingtalk.com>
2021-01-18 12:00:33 +0800

    用动画和实战打开 React Hooks（一）：useState 和 useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
+import GlobalStats from './components/GlobalStats'
+import CountriesChart from "./components/CountriesChart";
+import SelectDataKey from "./components/SelectDataKey";
 import './App.css';
 
+const baseUrl = 'https://corona.lmao.ninja/v2'
+
 function App() {
+  const [globalStats, setGlobalStats] = useState({})
+  const [countries, setCountries] = useState([])
+  const [key, setKey] = useState("case")
+
+  useEffect(() => {
+    const asyncGlobal = async() => {
+      const response = await fetch(`${baseUrl}/all`)
+      response.json().then(data => {
+        setGlobalStats(data)
+      })
+    }
+    asyncGlobal()
+    const intervalId = setInterval(asyncGlobal, 10000);
+    return () => clearInterval(intervalId)
+  }, [])
+
+  useEffect(() => {
+    const fetchCountries = async() => {
+      const response = await fetch(`${baseUrl}/countries?sort=${key}`)
+      const data = await response.json()
+      setCountries(data.slice(0,10))
+    };
+
+    fetchCountries()
+  }, [key])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>COVID-19</h1>
+      <GlobalStats  stats={globalStats} />
+      <SelectDataKey onChange={(e) => setKey(e.target.value)} />
+      <CountriesChart data={countries}  dataKey={key} />
     </div>
   );
 }
